1509991a7fee82a7871a48ccdbbe7e8aac9a784b
marty <marty.shu@gmail.com>
2023-05-08 08:25:29 +0800

    test

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from youtube import YoutubeLoader
 
-OUTPUT_DIR = 'test_01/'
+OUTPUT_DIR = './video/生物01/'
+URL = 'https://youtu.be/LHY3oyhlTsw'
+URL = 'https://www.youtube.com/watch?v=LHY3oyhlTsw'
 
-#dl = YoutubeLoader('https://www.youtube.com/watch?v=dZbqIXs9Z08', model='tiny')
-#dl.saveVideo(OUTPUT_DIR).saveSubtitles(OUTPUT_DIR).merge(OUTPUT_DIR)
+#dl = YoutubeLoader(URL, model='large')
+#dl.saveVideo(OUTPUT_DIR)
 
-dl = YoutubeLoader('./test_01/用 ChatGPT 學運算思維 (開字幕).mp4', model='large')
-dl.saveSubtitles(OUTPUT_DIR).merge(OUTPUT_DIR)
+dl = YoutubeLoader("./video/生物01/111AB402_01_03.mp4", model='large')
+dl.loadVideo()
+dl.saveSubtitles(OUTPUT_DIR)
+dl.merge(OUTPUT_DIR)
+
 print(dl.info())
