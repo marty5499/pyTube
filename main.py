385d9ba9419885954a718a2dbd62a5ce81c329a5
marty <marty.shu@gmail.com>
2023-08-08 01:11:28 +0800

    test get Youtube & convert

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 from youtube import YoutubeLoader
 
-OUTPUT_DIR = './video/生物01/'
+OUTPUT_DIR = './video/new/'
 URL = 'https://youtu.be/LHY3oyhlTsw'
 URL = 'https://www.youtube.com/watch?v=LHY3oyhlTsw'
+URL = 'https://www.youtube.com/watch?v=72bQ2CbED-Y' #新聞面對面
 
-#dl = YoutubeLoader(URL, model='large')
-#dl.saveVideo(OUTPUT_DIR)
+"""
+dl = YoutubeLoader(URL, model='large')
+dl.saveVideo(OUTPUT_DIR)
+"""
 
-dl = YoutubeLoader("./video/生物01/111AB402_01_03.mp4", model='large')
+dl = YoutubeLoader("./video/new/news.mp4", model='large')
 dl.loadVideo()
 dl.saveSubtitles(OUTPUT_DIR)
 dl.merge(OUTPUT_DIR)
-
 print(dl.info())
